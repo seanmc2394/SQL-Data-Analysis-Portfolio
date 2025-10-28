ec2be655b4c8ba4b47f9e17a562430ec45614519
Sean <seanmcdonnell001@gmail.com>
2025-10-28 15:05:47 +0000

    Create create_database.sql

---
+++ Hospital_Management_Database/create_database.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE hospital;
+USE hospital;
+
+
+CREATE TABLE doctors (
+    doctor_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    specialty VARCHAR(100),
+    phone_number VARCHAR(20));
+
+
+CREATE TABLE patients (
+    patient_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    gender VARCHAR(100),
+    phone_number VARCHAR(20));
+
+
+CREATE TABLE appointments (
+    appointment_id INT AUTO_INCREMENT PRIMARY KEY,
+    patient_id INT,
+    doctor_id INT,
+    appointment_date DATE,
+    diagnosis VARCHAR(255),
+    treatment VARCHAR(255),
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(doctor_id));
+
+
+CREATE TABLE departments (
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(100),
+    location VARCHAR(100));
+
+
+ALTER TABLE doctors
+ADD department_id INT,
+ADD FOREIGN KEY (department_id) REFERENCES departments(department_id);
